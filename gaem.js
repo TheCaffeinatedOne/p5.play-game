6d8e6a19dedd93691cc96d6411fb4732348c4edf
TheCaffienatedOne <thecaffinatedone@proton.com>
2020-10-28 22:34:35 -0400

    stuff

--- gaem.js
+++ gaem.js
@@ -1,18 +1,17 @@
+
+let eye;
 function setup() {
-	createCanvas(1350, 600);
-  spr = createSprite(
-    width/2, height/2, 50, 50);
-  spr.shapeColor = color(255);
-  spr.velocity.y;
-  spr.velocity.x;
+	createCanvas(600, 500);
+ 	eye = loadImage('eye.png', 'eye has loaded', 'eye didnt load');
+  plr = createSprite(width/2, height/2, 50, 50);
+  plr.velocity.y;
+  plr.velocity.x;
+	plr.addImage(eye);
 }
 
 
 function draw() {
 	background(0);
-
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
   drawSprites();
 }
 
@@ -20,19 +19,19 @@ function keyPressed() {
 	switch(keyCode) {
 		case 37:
 		case 65:
-			spr.velocity.x = -5;
+			plr.velocity.x = -5;
 			break;
 		case 39:
 		case 68:
-			spr.velocity.x = 5;
+			plr.velocity.x = 5;
 			break;
 		case 38:
 		case 87:
-			spr.velocity.y = -5;
+			plr.velocity.y = -5;
 			break;
 		case 40:
 		case 83:
-			spr.velocity.y = 5;
+			plr.velocity.y = 5;
 			break;
 	}
 }
@@ -41,19 +40,19 @@ function keyReleased() {
 	switch(keyCode) {
 		case 37:
 		case 65:
-			spr.velocity.x = 0;
+			plr.velocity.x = 0;
 			break;
 		case 39:
 		case 68:
-			spr.velocity.x = 0;
+			plr.velocity.x = 0;
 			break;
 		case 38:
 		case 87:
-			spr.velocity.y = 0;
+			plr.velocity.y = 0;
 			break;
 		case 40:
 		case 83:
-			spr.velocity.y = 0;
+			plr.velocity.y = 0;
 			break;
 	}
 }
