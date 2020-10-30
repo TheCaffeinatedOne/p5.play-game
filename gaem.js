aaae8a5994e0bb136547739abec1edcb003e9a06
TheCaffienatedOne <thecaffinatedone@proton.com>
2020-10-29 22:27:59 -0400

    bruhmoment

--- gaem.js
+++ gaem.js
@@ -11,7 +11,8 @@ function setup() {
  	sprOne = createSprite(width/2, height/2, 50, 50);
   sprOne.velocity.y;
   sprOne.velocity.x;
-	sprOne.addAnimation("bruh", walkOne);
+	sprOne.addImage(bill);
+
 }
 function draw() {
 	background(255);
@@ -24,19 +25,23 @@ function keyPressed() {
 		case 65:
 			sprOne.velocity.x = -3.5;
 			sprOne.mirrorX(-1);
+			sprOne.addAnimation("bruh", walkOne);
 			break;
 		case 39:
 		case 68:
 			sprOne.velocity.x = 3.5;
 			sprOne.mirrorX(1);
+			sprOne.addAnimation("bruh", walkOne);
 			break;
 		case 38:
 		case 87:
 			sprOne.velocity.y = -3.5;
+			sprOne.addAnimation("bruh", walkOne);
 			break;
 		case 40:
 		case 83:
 			sprOne.velocity.y = 3.5;
+			sprOne.addAnimation("bruh", walkOne);
 			break;
 	}
 }
@@ -47,19 +52,23 @@ function keyReleased() {
 		case 65:
 			sprOne.velocity.x = 0;
 			sprOne.mirrorX(-1);
+			sprOne.addImage(bill);
 			break;
 		case 39:
 		case 68:
 			sprOne.velocity.x = 0;
 			sprOne.mirrorX(1);
+			sprOne.addImage(bill);
 			break;
 		case 38:
 		case 87:
 			sprOne.velocity.y = 0;
+			sprOne.addImage(bill);
 			break;
 		case 40:
 		case 83:
 			sprOne.velocity.y = 0;
+			sprOne.addImage(bill);
 			break;
 	}
 }
